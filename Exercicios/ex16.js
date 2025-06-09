2a432a98379ce354fe60709ef544ad55fb797db2
João Pedro Ledur Favero <joao.favero@inova.local>
2025-06-09 15:46:54 -0300

    Add ex016

---
+++ Exercicios/ex16.js
@@ -0,0 +1,35 @@
+let numeros = new Set()
+
+function geraVetorAleatorio(){
+
+    let n = Math.floor(Math.random()*1000) + 1
+    if(!numeros.has(n)){
+        numeros.add(n)
+    }
+    
+    if(numeros.size === 20){
+        let vet = Array.from(numeros)
+        ordenarValores(vet)
+    }
+    else{
+        setTimeout(geraVetorAleatorio,100)
+    }
+}
+
+function ordenarValores(vet){
+    let aux
+        for(let i = 0; i < vet.length; i++){
+        for(j = 0; j < vet.length ; j++){
+            if(vet[j] > vet[j+1]){
+                aux = vet[j]
+                vet[j] = vet[j+1]
+                vet[j+1]= aux
+            }
+        }
+    }
+    
+    console.table(vet)
+}
+
+
+geraVetorAleatorio()
